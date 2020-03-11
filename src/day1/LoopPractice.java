0b3fbfaac09fde6ab54c4b33ea7297afaa8631d6
samilAkkus <samilakgus15@email.com>
2020-03-11 16:41:44 -0700

    new

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -3,13 +3,11 @@ package day1;
 public class LoopPractice {
 
     public static void main(String[] args) {
-
-
-        for (int i = 0; i < 10 ; i+=2) {
-
-            System.out.println(i);
-            System.out.println("########");
-
+        //some random code
+        for (int i=0; i <10; i+=2){
+            System.out.println("Counter :: "+i);
+            System.out.println("####");
+            System.out.println("git is fun:(");
         }
     }
 }
--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,10 +3,8 @@ package day1;
 public class StringPractice {
 
     public static void main(String[] args) {
-
         System.out.println("Hello, world!");
+        String str = "Java is fun!";
+        System.out.println(str);
     }
-
-    String str = "samil";
-
 }
