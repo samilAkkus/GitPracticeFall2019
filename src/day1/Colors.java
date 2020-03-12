c113814782575185b006973a1548d8665bc463b2
samilAkkus <samilakgus15@email.com>
2020-03-11 17:12:42 -0700

    modified array of colors

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -4,7 +4,7 @@ public class Colors {
 
     public static void main(String[] args) {
 
-        String[] colors = {"red","blue","green"};
+        String[] colors = {"red","blue","green","yellow"};
 
     }
 }
